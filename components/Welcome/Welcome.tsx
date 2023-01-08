04e6cc801f096da11d2e1792b81215395df2fb1b
crypt0grapher <7blockchains@gmail.com>
2023-01-08 16:22:05 +0800

    feat:  page with address inputline and network connection

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -7,19 +7,28 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} align="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span">
-          Mantine
+        NFT{' '}
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{
+            from: 'teal',
+            to: 'lime',
+            deg: 105,
+          }}
+        >
+          Snapshot
         </Text>
       </Title>
-      <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit index.tsx file.
-      </Text>
+      {/*<Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">*/}
+      {/*  This starter Next.js project includes a minimal setup for server side rendering, if you want*/}
+      {/*  to learn more on Mantine + Next.js integration follow{' '}*/}
+      {/*  <Anchor href="https://mantine.dev/guides/next/" size="lg">*/}
+      {/*    this guide*/}
+      {/*  </Anchor>*/}
+      {/*  . To get started edit index.tsx file.*/}
+      {/*</Text>*/}
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,90 @@
-import { Welcome } from '../components/Welcome/Welcome';
+import {
+  Input,
+  Button,
+  Group,
+  Badge,
+  TextInput,
+  TextInputProps,
+  ActionIcon,
+  useMantineTheme,
+  Stack,
+  Loader,
+} from '@mantine/core';
+import { IconCurrencyEthereum, IconArrowRight, IconArrowLeft } from '@tabler/icons';
+import { useCallback, useState, useEffect } from 'react';
+import Link from 'next/link';
+import { isAddress } from 'ethers/lib/utils';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Welcome } from '../components/Welcome/Welcome';
+import { hooks, network } from '../connectors/network';
+
+const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider, useENSNames } = hooks;
 
 export default function HomePage() {
+  const theme = useMantineTheme();
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const isActivating = useIsActivating();
+
+  const isActive = useIsActive();
+
+  const provider = useProvider();
+  const ENSNames = useENSNames(provider);
+  const [error, setError] = useState(undefined);
+  const [address, setAddress] = useState('');
+  const [contractDetails, setContractDetails] = useState({
+    contractAddress: '',
+    totalSupply: -1,
+    owners: -1,
+  });
+  const [gettingContractDetails, setGettingContractDetails] = useState(undefined);
+
+  const [fetchingTokens, setfetchingTokens] = useState(undefined);
+
+  useEffect(() => {
+    void network.activate().catch(() => {
+      console.debug('Failed to connect to network');
+    });
+  }, []);
+
+  const onActionIconClick = useCallback(() => {
+    setGettingContractDetails(true);
+  }, []);
   return (
-    <>
+    <Stack align="center" spacing="xl">
       <Welcome />
+      <Badge color={isActive ? 'lime' : 'red'} size="xl" variant="dot">
+        Ethereum is {isActivating ? 'connecting' : isActive ? 'connected' : 'not connected'}
+      </Badge>
       <ColorSchemeToggle />
-    </>
+      <TextInput
+        style={{ minWidth: '600px' }}
+        maxLength={'0x746a94728a7188b6218e640e15290bacb1c4d56a'.length}
+        icon={<IconCurrencyEthereum size={18} stroke={1.5} />}
+        radius="xl"
+        size="xl"
+        value={address}
+        onChange={(event) => setAddress(event.currentTarget.value)}
+        error={!address || isAddress(address) ? undefined : 'Invalid address'}
+        rightSection={
+          <ActionIcon
+            size={32}
+            radius="xl"
+            color="lime"
+            variant="filled"
+            onClick={onActionIconClick}
+          >
+            {isActive && !gettingContractDetails ? (
+              <IconArrowRight size={18} stroke={1.5} />
+            ) : (
+              <Loader size={18} color={theme.white} />
+            )}
+          </ActionIcon>
+        }
+        placeholder="NFT Smart Contract Ethereum Address"
+        rightSectionWidth={62}
+      />
+      <Group position="center" />
+    </Stack>
   );
 }
